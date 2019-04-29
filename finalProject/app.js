c6354cfd74804d54de213c58d36ad49afd7611ad
WalkerTxsRngr7 <walkergross7@gmail.com>
2019-04-29 16:17:34 -0500

    initial commit final project

---
+++ finalProject/app.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//     $("#heading").html("Card Deck")
+//     $.getJSON(
+//         "https://api.themoviedb.org/3/movie/now_playing?api_key=ab8a8893b0afa6fdc8643b146b27b9f3",
+//         function (movie) {
+//             $("#deck").html(movie);
+//             for (i = 0; i < 3; i++) {
+//                 $("div#deck").append('<movie title=movie.results[i].title></movie>');
+//             }
+//         })
+
+// })
